5bafe2de8d012eb6531e30f495753372fd47f298
Aurélia <aurelia.ngau@gmail.com>
2020-11-21 15:26:30 +0100

    Login page

--- client/components/Login.vue
+++ client/components/Login.vue
@@ -1,8 +1,9 @@
 <template>
+<div class="cool">
   <div class = "test">
     <div class="col">
             <div class="hide-md-lg">
-              <p>Login: </p>
+              <h2>Login: </h2>
             </div>
             <div v-if = "userId === null">
               <div class="sign">
@@ -12,16 +13,18 @@
                 <input type="password" name="password" placeholder="Password" v-model = "userPassword" required>
               </div>
               <div class="sign">
-                <input type="submit" value="Login" @click="loginUser(userEmail, userPassword, userId);">
+                <input class="buttonSignIn" type="submit" value="Login" @click="loginUser(userEmail, userPassword, userId);">
               </div>
               <a href = "index.html#/register" class = "navigation">NOT REGISTERED YET ?</a>
             </div>
-                
+             <!--
             <div v-else>
                 <p> You are connected </p>
-            </div>
+            </div>-->
       </div>
     </div>
+   <div class="tee"></div>
+</div>   
 </template>
 
 <script>
@@ -53,35 +56,84 @@ module.exports = {
 
 <style scoped>
 .test{
-  
-  margin-top: 5%;
-  background: url(../images/avion.jpg) no-repeat;
-  width: 100%;
+  background: url(../images/r.jpg) no-repeat;
   background-size: cover;
-  height: 80vh;
-  opacity: 87%;
+  margin-top: 9vh;
+  width: 100%;
+  height: 70vh;
+  border-bottom: 20vh;
 }
 
 .col{
-  
-  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 26%;
+  margin: auto;
+  background-color:rgb(0, 0, 0, 0.1);
+  color: white;
+  padding: 20px;
+  box-shadow: 0px 2px 8px 2px #555;
+  border-radius: 6px;
+  box-sizing: border-box;
+}
+
+.col h2{
   text-align: center;
-  
-  
+  letter-spacing: 1px;
+  font-family: 'Arbutus Slab';
+  color: white;
+  font-size: 35px;
+  margin-bottom: 50px;
 }
 
-.hide-md-lg{
-  font-family: 'Playfair Display', serif;
-  color:#ebede4;
-  font-size:55px;
-  font-weight: 200;
-  text-align: center;
+.col{
+  padding: 40px;;
 }
 
-
-.sign{
-  margin-top: 20px;
+input{
+  width: 100%;
+  padding: 8px 12px;
+  margin: 8px 0px;
+  display: inline-block;
+  box-sizing: border-box;
+  background-color: transparent;
+  border: 1px solid white;
+  margin-bottom: 30px;
+  font-size:18px;
 }
 
+input:focus{
+  outline: none;
+}
+
+.buttonSignIn{
+  padding: 12px 20px;
+  cursor: pointer;
+  font-size: 14px;
+  letter-spacing: 1px;
+  font-size:18px;
+  color: white;
+}
+
+.buttonSignIn:hover{
+  background: rgba(0,0,0,0.5);
+}
+
+.tee{
+  background-color: white;
+  height: 10vh;
+
+}
+
+::placeholder {
+  color: #dce3e6;
+}
+
+.navigation{
+  color: #eaf2cb;
+  transition: all 0.3s ease 0s;
+}
 
 </style>
